7d46c32e5e3dbc6d543957dec81c8620112b9263
dglee <slsldkf123@naver.com>
2021-08-11 23:47:03 +0900

    AppConfig 역할 분리

--- src/main/java/com/devhistory/springcoreprinciple/AppConfig.java
+++ src/main/java/com/devhistory/springcoreprinciple/AppConfig.java
@@ -1,6 +1,9 @@
 package com.devhistory.springcoreprinciple;
 
+import com.devhistory.springcoreprinciple.discount.DiscountPolicy;
 import com.devhistory.springcoreprinciple.discount.FixDiscountPolicy;
+import com.devhistory.springcoreprinciple.discount.RateDiscountPolicy;
+import com.devhistory.springcoreprinciple.member.MemberRepository;
 import com.devhistory.springcoreprinciple.member.MemberService;
 import com.devhistory.springcoreprinciple.member.MemberServiceImpl;
 import com.devhistory.springcoreprinciple.member.MemoryMemberRepository;
@@ -9,12 +12,24 @@ import com.devhistory.springcoreprinciple.order.OrderServiceImpl;
 
 
 public class AppConfig {
-    //생성자 주입
+    //memberService 역할, 생성자 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-    //생성자 주입
+
+    //orderService 역할, 생성자 주입
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //memberRepository 역할
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    //discountPolicy 역할
+    public DiscountPolicy discountPolicy() {
+//      return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
