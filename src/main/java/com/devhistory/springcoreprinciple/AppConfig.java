2c2759801d99b6993c596755b0c1bf6b46cc1849
dglee <slsldkf123@naver.com>
2021-08-12 22:53:35 +0900

    AppConfig 스프링으로 전환

--- src/main/java/com/devhistory/springcoreprinciple/AppConfig.java
+++ src/main/java/com/devhistory/springcoreprinciple/AppConfig.java
@@ -9,25 +9,33 @@ import com.devhistory.springcoreprinciple.member.MemberServiceImpl;
 import com.devhistory.springcoreprinciple.member.MemoryMemberRepository;
 import com.devhistory.springcoreprinciple.order.OrderService;
 import com.devhistory.springcoreprinciple.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+//스프링 구성정보(해당 구성 정보들이 스프링 컨테이너에 등록된다. 구성 정보는 여러개 존재할 수 있다.)
+@Configuration
 public class AppConfig {
+
     //memberService 역할, 생성자 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     //orderService 역할, 생성자 주입
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     //memberRepository 역할
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     //discountPolicy 역할
+    @Bean
     public DiscountPolicy discountPolicy() {
 //      return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/com/devhistory/springcoreprinciple/MemberApp.java
+++ src/main/java/com/devhistory/springcoreprinciple/MemberApp.java
@@ -4,12 +4,23 @@ import com.devhistory.springcoreprinciple.member.Grade;
 import com.devhistory.springcoreprinciple.member.Member;
 import com.devhistory.springcoreprinciple.member.MemberService;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        /*
+            스프링 컨테이너 생성(애노테이션 기반으로 구성 설정)
+            - 인자로 전달한 AppConfig의 환경설정 정보를 컨테이너 널어서 관리(Bean 등록)
+            - 기본적으로 메소드 이름으로 Bean이 등록된다.
+         */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//스프링 컨테이너에서 타입이 'MemberService', 이름이 'memberService'인 Bean 꺼내기
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
