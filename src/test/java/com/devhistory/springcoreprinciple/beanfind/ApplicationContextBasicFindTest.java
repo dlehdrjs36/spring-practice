669f9dd185f15f6a98d6b5d2871f7fcb65f4fcfc
dglee <slsldkf123@naver.com>
2021-08-15 00:06:32 +0900

    스프링 컨테이너 빈 조회 테스트 추가

---
+++ src/test/java/com/devhistory/springcoreprinciple/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,51 @@
+package com.devhistory.springcoreprinciple.beanfind;
+
+import com.devhistory.springcoreprinciple.AppConfig;
+import com.devhistory.springcoreprinciple.member.MemberService;
+import com.devhistory.springcoreprinciple.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        //구체 타입으로 조회하면 변경시 유연성이 떨어진다.
+        //Appconfig 클래스의 구성 정보의 반환타입(MemberService memberService())과 관계없이 스프링 컨테이너에 등록된 인스턴스의 타입(new MemberServiceImpl(memberRepository()))으로 결정된다.
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        //ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
