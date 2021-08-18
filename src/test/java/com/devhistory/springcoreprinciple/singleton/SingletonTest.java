aad674229b4903b2884771cec31456345a607c40
dglee <slsldkf123@naver.com>
2021-08-19 00:00:36 +0900

    싱글톤 객체생성 테스트 추가

--- src/test/java/com/devhistory/springcoreprinciple/singleton/SingletonTest.java
+++ src/test/java/com/devhistory/springcoreprinciple/singleton/SingletonTest.java
@@ -2,11 +2,12 @@ package com.devhistory.springcoreprinciple.singleton;
 
 import com.devhistory.springcoreprinciple.AppConfig;
 import com.devhistory.springcoreprinciple.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonTest {
 
@@ -42,4 +43,19 @@ public class SingletonTest {
         //equal : equals()를 오버라이드한 것과 같이 값을 비교
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
