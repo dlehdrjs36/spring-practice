51f39ceb1aba97db9d09d825a615e20a36e08d97
dglee <slsldkf123@naver.com>
2021-08-24 23:26:06 +0900

    컴포넌트 스캔 중복등록 설정 추가

--- src/main/java/com/devhistory/springcoreprinciple/AutoAppConfig.java
+++ src/main/java/com/devhistory/springcoreprinciple/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.devhistory.springcoreprinciple;
 
+import com.devhistory.springcoreprinciple.member.MemberRepository;
+import com.devhistory.springcoreprinciple.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +14,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
