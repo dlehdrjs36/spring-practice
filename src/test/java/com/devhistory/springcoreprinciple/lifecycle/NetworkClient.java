1ed0e7cdaa71326be80901e67bd827547ea737e0
dglee <slsldkf123@naver.com>
2021-08-30 23:36:18 +0900

    빈 생명주기 콜백 설정 수정

--- src/test/java/com/devhistory/springcoreprinciple/lifecycle/NetworkClient.java
+++ src/test/java/com/devhistory/springcoreprinciple/lifecycle/NetworkClient.java
@@ -1,5 +1,11 @@
 package com.devhistory.springcoreprinciple.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //네트워크 연결, 종료를 한다고 가정
 public class NetworkClient {
 
@@ -7,8 +13,6 @@ public class NetworkClient {
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,5 +31,20 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    //의존관계 주입까지 완료 후 초기화 수행
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //소멸 전 수행
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
