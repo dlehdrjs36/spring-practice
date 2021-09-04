46db86f38aced3e977637ac0872ad7c18c656262
dglee <slsldkf123@naver.com>
2021-09-04 23:27:23 +0900

    웹스코프 프로바이더 예제 추가

--- src/main/java/com/devhistory/springcoreprinciple/web/LogDemoController.java
+++ src/main/java/com/devhistory/springcoreprinciple/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.devhistory.springcoreprinciple.web;
 
 import com.devhistory.springcoreprinciple.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
-        myLogger.log("controller test");
 
+        myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
     }
